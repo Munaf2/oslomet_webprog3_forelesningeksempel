5482578c0f91b426e11d9435478c548669e458f5
Munaf2 <142993551+Munaf2@users.noreply.github.com>
2024-02-15 10:26:44 +0100

    Initial commit

---
+++ src/main/java/com/example/oslomet_webprog3_forelesningeksempel/OslometWebprog3ForelesningeksempelApplication.java
@@ -0,0 +1,13 @@
+package com.example.oslomet_webprog3_forelesningeksempel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OslometWebprog3ForelesningeksempelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OslometWebprog3ForelesningeksempelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oslomet_webprog3_forelesningeksempel/OslometWebprog3ForelesningeksempelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oslomet_webprog3_forelesningeksempel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OslometWebprog3ForelesningeksempelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
